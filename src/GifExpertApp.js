1f160610b24b2e11d8a7129320fe51d57df3bcc3
rjvelazco <rjvelazco21@gmail.com>
2021-10-04 19:50:17 -0400

    add GifGridItem component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,7 +4,7 @@ import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Piece']);
+    const [categories, setCategories] = useState(['Fire Force']);
 
     return (
         <>
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { GifGridItem } from '../GifGridItem';
+
+// Api key for Giphy
+const api_key = 'GyTgDRvJi4SIjxKQSRDSKA5CNussIOTV';
 
 export const GifGrid = ({ category }) => {
-    const api_key = 'GyTgDRvJi4SIjxKQSRDSKA5CNussIOTV';
     
+    const [ images, setImages ] = useState([]);
+
+    useEffect(() => {
+        getGifs();
+    }, []);
+
     const getGifs = async () => {
 
         const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api_key}`;
@@ -16,18 +25,21 @@ export const GifGrid = ({ category }) => {
                 url: img.images.downsized_medium.url
             }
         })
-
-        return gifs;
+        setImages(gifs);
     }
 
-    useEffect(() => {
-        getGifs()
-            .then( resp => console.log(resp));
-    }, [])
-    getGifs();
     return (
         <div>
             <h3>{ category }</h3>
+            { 
+                images.map((img) => (
+                    <GifGridItem
+                        key={img.id}
+                        {...img}
+                    />
+                    // img={img}
+                ))
+            }
         </div>
     )
 }
