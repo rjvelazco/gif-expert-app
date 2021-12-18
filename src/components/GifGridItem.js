9c197d6aab813c7ba3df42e60ac7eb8af2a2273f
rjvelazco <rjvelazco21@gmail.com>
2021-12-17 20:52:09 -0400

    GrifGridItem tests

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export const GifGridItem = ({category, title, url}) => {
-    const repTitle =  `${category } - gif`.toUpperCase();
+    const repTitle =  `${ category } - gif`.toUpperCase();
     return (
         <div className="card">
             <img src={url} alt={title} />
             <p>{ title ? title : repTitle }</p>
         </div>
     )
+}
+
+GifGridItem.prototype = {
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
 }
---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { GifGridItem } from '../../components/GifGridItem';
+
+describe('Tests in <GifGridItem />', () => {
+
+    const title = 'Title';
+    const url = 'https://localhost/algo.jpg';
+    const category = 'Anime';
+    const wrapper = shallow(
+        <GifGridItem title={title} url={url} category={category} />
+    );
+
+
+    test("It should render GrifGridItem Component Correctly", () => {
+
+        // wrapper.setProps({ title: 'setProps Methods' });
+        // wrapper.update();
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test("It should have a <p> with the title", () => {
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe(title);
+    });
+
+    test("It should have a <img> with the url and alt props value", () => {
+        const img = wrapper.find('img');
+        // console.log( img.props() );
+        // img.props() -> Todos los props;
+        // img.prop('porpName') -> Solo el prop especificado.
+        expect( img.props().src ).toBe(url);
+        expect( img.prop('alt') ).toBe(title);
+    });
+
+    test("It should have a <div> with the class 'card'", () => {
+        const div = wrapper.find('div');
+        // console.log(div.props());
+        expect( div.prop('className') ).toContain('card');
+    })
+
+});
